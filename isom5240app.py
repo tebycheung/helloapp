7a50718fb20913fbe4ba0e2ace24d5317a6f747f
tebycheung <sweetl_tea@yahoo.com.hk>
2026-02-28 22:49:34 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,22 +1,19 @@
-from transformers import pipeline
-from PIL import Image
-import streamlit as st
+# function part
+# img2text
+def img2text(url):
+    image_to_text_model = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    text = image_to_text_model(url)[0]["generated_text"]
+    return text
 
-# Streamlit UI
-st.header("Title: Age Classification using ViT")
+# text2story
+def text2story(text):
+    story_text = ""   # to be completed
+    return story_text
 
-# Load the age classification pipeline
-# The code below should be placed in the main part of the program
-age_classifier = pipeline("image-classification", model="prithivMLmods/Age-Classification-SigLIP2")
+# text2audio
+def text2audio(story_text):
+    audio_data = ""     # to be completed
+    return audio_data
 
-image_name = "middleagedMan.jpg"
-image_name = Image.open(image_name).convert("RGB")
-
-# Classify age
-age_predictions = age_classifier(image_name)
-st.write(age_predictions)
-age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
-
-# Display results
-st.write("Predicted Age Range:")
-st.write(f"Age range: {age_predictions[0]['label']}")
+# main part
+# ... to be completed
